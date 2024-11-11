40bad2c89f61bade79c3cfeb110935bc0a48aca1
MaNiaCode <WhereMania@gmail.com>
2024-11-11 12:45:46 +0330

    fixed auth and redirect + flask_login

--- Market/__init__.py
+++ Market/__init__.py
@@ -2,8 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from os import path
+from flask_login import LoginManager
+
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 migrate = Migrate()
 
 DB_USER = "newuser"
@@ -18,11 +21,16 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+    login_manager.init_app(app)
     migrate.init_app(app, db)
 
+    login_manager.login_view = "auth.login"
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
     from .models import User , Note , Item
-
-
     from .home import home
     from .market import market
     from .tasks import tasks
--- Market/auth.py
+++ Market/auth.py
@@ -1,6 +1,7 @@
 from flask import render_template , session , Blueprint , request , flash , redirect , url_for
 from . import db
 from .models import User
+from flask_login import login_user , logout_user , login_required
 
 auth = Blueprint("auth",__name__)
 
@@ -9,18 +10,18 @@ auth = Blueprint("auth",__name__)
 @auth.route('/register', methods=["POST" , "GET"])
 def register():
     if request.method == "POST":
-        email = request.form('email')
-        password1 = request.form('password1')
-        password2 = request.form('password2')
-        first_name = request.form('first_name')
-
+        first_name = request.form.get('first_name')
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
 
         if password1 != password2:
             flash("Passwords do not match", category='error')
             render_template('register.html')
         
         if User.query.filter_by(email=email).first():
-            flash("Username already exists.", category='error')
+            flash("Email already exists.", category='error')
 
         new_user = User(email=email , first_name=first_name)
         new_user.set_password(password1)
@@ -31,6 +32,27 @@ def register():
         return redirect(url_for('auth.login'))
     
     return render_template('register.html')
-@auth.route('/login')
+
+@auth.route('/login', methods=["POST" , "GET"])
 def login():
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user and user.check_password(password):
+            login_user(user)
+            flash('Logged in successfully', category='success')
+            return redirect(url_for('home.home_page'))
+        else:
+            flash('Invalid email or password ', category='error')
+            return render_template('login.html')
+
     return render_template("login.html")
+
+@auth.route('logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out succsessfuly", category='success')
+    return redirect(url_for('auth.login'))
